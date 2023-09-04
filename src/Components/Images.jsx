7bf09e015a10762525eb438b5e8747adebf0b5c7
Pujarini Sahoo <pujarinisahoo95@gmail.com>
2023-09-05 00:33:24 +0530

    changes

--- src/Components/Images.jsx
+++ src/Components/Images.jsx
@@ -7,7 +7,7 @@ const Images = () => {
   const [image, setImage] = useState(
     "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
   );
-  const [inputVal, setInputVal] = useState("");
+  const [inputVal, setInputVal] = useState("Work is Life");
   const [inputWidth, setInputWidth] = useState(inputVal.length || 0);
 
   // useEffect(() => {
