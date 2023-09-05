b7d43043f825c069ac9b37dc8a258174c24a6160
Pujarini Sahoo <pujarinisahoo95@gmail.com>
2023-09-05 12:09:00 +0530

    changes

--- src/Components/Images.jsx
+++ src/Components/Images.jsx
@@ -4,19 +4,17 @@ import styles from "../Styles/Image.module.css";
 import Draggable from "react-draggable";
 
 const Images = () => {
-  const [image, setImage] = useState(
-    "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
-  );
+  const [image, setImage] = useState("");
   const [inputVal, setInputVal] = useState("Work is Life");
   const [inputWidth, setInputWidth] = useState(inputVal.length || 0);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       `https://api.unsplash.com/photos/random?orientation=landscape&client_id=jx4QHv5IzN5BPiGHxszR_Amu11Cx81ggIUG8q26_cc0`
-  //     )
-  //     .then((res) => setImage(res?.data?.urls?.full));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.unsplash.com/photos/random?orientation=landscape&client_id=jx4QHv5IzN5BPiGHxszR_Amu11Cx81ggIUG8q26_cc0`
+      )
+      .then((res) => setImage(res?.data?.urls?.full));
+  }, []);
 
   useEffect(() => {
     setInputWidth(inputVal.length * 15);
@@ -34,7 +32,6 @@ const Images = () => {
             value={inputVal}
             className={styles.dragInput}
             onChange={(e) => {
-              console.log(e.target.value);
               setInputVal(e.target.value);
             }}
             style={{ width: `${inputWidth}px` }}
@@ -46,3 +43,5 @@ const Images = () => {
 };
 
 export default Images;
+
+//https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80
